01547add13d6e6b82f96c5e733f57968c6a9ede5
Mama679 <ingmariomanotas@hotmail.com>
2023-09-01 14:41:08 -0500

    Home Frontent realizado

--- app-tutofox/src/app/app-routing.module.ts
+++ app-tutofox/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-export const router: Routes =[];
+export const router: Routes =[
+  {
+    path:'',
+    loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path:'',
+    redirectTo:'/',
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    redirectTo:'error/404'
+  }
+];
 
 @NgModule({
   imports: [
--- app-tutofox/src/app/app.module.ts
+++ app-tutofox/src/app/app.module.ts
@@ -6,12 +6,11 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './modules/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
