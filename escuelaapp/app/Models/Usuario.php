f725931c3aa5ffa2721100ea8e4e8aa6786af520
Mama679 <ingmariomanotas@hotmail.com>
2023-09-01 16:35:24 -0500

    migraciones backend

---
+++ escuelaapp/app/Models/Usuario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+    protected $table = 'Usuarios';
+    protected $primaryKey = 'id_usuario';
+    public $incrementing = true;
+    //public $timestamps = true;
+
+    const CREATED_AT = 'fecha_crea';
+    const UPDATED_AT = 'fecha_mod';
+}
