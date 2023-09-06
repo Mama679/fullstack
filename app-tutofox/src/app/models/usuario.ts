7525e65da9b7bcd5326f92ef83a21f3cca5a0c09
Mama679 <ingmariomanotas@hotmail.com>
2023-09-06 16:00:18 -0500

    servicio login de usuario

---
+++ app-tutofox/src/app/models/usuario.ts
@@ -0,0 +1,8 @@
+export interface Usuario {
+    id:number;
+    usernom:string;
+    nombres:string;
+    email:string;
+    password:string;
+    rol_id:number;
+}
--- app-tutofox/src/app/modules/service/auth.service.ts
+++ app-tutofox/src/app/modules/service/auth.service.ts
@@ -1,14 +1,33 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 import { Router } from '@angular/router';
 import {catchError, map} from 'rxjs/operators';
-import {of} from 'rxjs';
+import {of,Observable, throwError} from 'rxjs';
+
 import { URL_SERVICIO } from 'src/app/config/config';
+import { Usuario } from 'src/app/models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  token:any = null;
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept':'application/json'
+    })
+ }
+
+  constructor(
+    private http:HttpClient,
+    private router:Router
+  ) { }
+
+  login(usuario:string, clave:string){
+    let url = URL_SERVICIO + "api/login";
+    return this.http.post();
+  }
 }
