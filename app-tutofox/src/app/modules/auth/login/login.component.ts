f828a5790193c920daeb97712ee0ffa62df72de6
Mama679 <ingmariomanotas@hotmail.com>
2023-09-19 18:01:51 -0500

    Validar mensajes el componente login

--- app-tutofox/src/app/modules/auth/login/login.component.ts
+++ app-tutofox/src/app/modules/auth/login/login.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+ 
+  username:string = "";
+  password:string = "";
 
+  constructor(
+    public authService: AuthService,
+    public router: Router) {
+ 
+    
+  }
+  
+
+  ngOnInit(): void {
+   
+  }
+
+  login() {
+
+    const user = {username: this.username, password:this.password};
+    this.authService.login(user).subscribe((resp:any) =>{
+      console.log(resp);
+       if(!resp.error)
+       {
+          console.log(resp);
+       }
+       else
+       {
+          if(!resp.error.status)
+          {
+            console.log(resp.error.mensaje);           
+          }
+          else
+          {
+            console.log(resp.error);
+          }
+         
+       }
+    });    
+  }
+
+ 
 }
--- app-tutofox/src/app/modules/auth/service/auth.service.ts
+++ app-tutofox/src/app/modules/auth/service/auth.service.ts
@@ -2,8 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router} from '@angular/router';
 import {catchError, map} from 'rxjs/operators';
-import {of} from 'rxjs';
+import {of,Observable, throwError} from 'rxjs';
 import { URL_SERVICIO } from 'src/app/config/config';
+import { Usuario } from 'src/app/models/usuario';
 
 
 @Injectable({
@@ -11,5 +12,41 @@ import { URL_SERVICIO } from 'src/app/config/config';
 })
 export class AuthService {
 
-  constructor() { }
+  user:any = null;
+  token:any = null;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router) { }
+
+  login(user:any): Observable<any>{
+    let url = URL_SERVICIO + "/login";
+    return this.http.post(url,{ usernom:user.username,password:user.password }).pipe(
+      map((resp:any) =>{
+        return resp;
+      }),
+      catchError((error:any) =>{
+        return of(error);
+      })
+    );
+  }
+
+  setLocalStorage(token:string, user:Usuario){
+    localStorage.setItem("token",token);
+    localStorage.setItem("usuario",JSON.stringify(user));
+  }
+
+  getLocalStorage(){
+    if(localStorage.getItem("token"))
+    {
+      this.token = localStorage.getItem('token');
+      this.user = JSON.parse(localStorage.getItem('usuario') ?? '');
+    }
+    else{
+      this.user = null;
+      this.token = null;
+    }
+  }
+
+
 }
